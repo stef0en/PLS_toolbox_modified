0c91f72350068306a8bb7c80da9fcec2da417f06
Steffen Wiegert <wiegert@mpib-berlin.mpg.de>
2016-04-21 13:47:32 +0200

    initialize functions: batch_create_datamat.m (untouched), batch_plsgui.m (already modified from 'matrix input') and working directory

---
+++ batch_subject_datamat_from_matrix/batch_plsgui.m
@@ -0,0 +1,34 @@
+function batch_plsgui(varargin)
+%% start PLS analysis or PLS datamat file creation.
+% hacked version: receive input from .mat file as structure not as text 
+% file
+
+
+   if nargin < 1
+      error('Usage: batch_plsgui(batch_text_file_name(s))');
+   end
+
+   %% initalize PLS
+   p = which('plsgui');
+   [p f] = fileparts(p); [p f] = fileparts(p);
+   cmdloc = fullfile(p, 'plscmd');
+   addpath(cmdloc);
+
+   %% loop over various input files
+   for i = 1:nargin
+      load(varargin{i});
+            
+      % test if analysis or datamat creation has to be started 
+      if batch_file.is_analysis==true
+         batch_pls_analysis(batch_file);
+      else
+          
+          %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+          %% NOT DONE YET!!!!
+          batch_create_datamat(batch_file);
+          %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+      end
+   end
+
+   return;					% batch_plsgui
+
