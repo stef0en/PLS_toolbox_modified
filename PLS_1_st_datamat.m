bb1342a5a88d92da9d913c3b1b609327c09c9add
Steffen Wiegert <wiegert@mpib-berlin.mpg.de>
2016-05-12 14:37:49 +0200

    PLS_1_st_datmat.m: - fixing batch file path and name evaluation - load batch file structure to a different variable - changing feedback

--- PLS_1_st_datamat.m
+++ PLS_1_st_datamat.m
@@ -4,19 +4,21 @@ function PLS_1_st_datamat( varargin )
 %
 % Input: 1. Subject ID list
 %        2. Template batch file
-%        3. Functional image location (optinal)
+%        3. Functional image directory
 %        4. Save directory (optinal)
+%           - If not set, files are saved to current working directory 
+%
+% Output: st_datamat ID specific
+%
+% takes template creates ID specific datamat file, seperate TASK-REST, RUN
   
 
 
 %% Test input and set paths
 if nargin == 0;
     error ('Input: arguments missing')
-elseif nargin == 1
-    error ('Input: ID list or template file path missing')
-elseif nargin == 2
-    subjID = varargin{1};
-    BATCHFILE = varargin{2};    
+elseif nargin < 3
+    error ('Input: ID list, template file path or Nifti path is missing')
 elseif nargin == 3
     subjID = varargin{1};
     BATCHFILE = varargin{2};
@@ -32,15 +34,7 @@ end
 
 if exist(BATCHFILE,'file') ~= 2
     error ('template file path incorrect')
-else load (BATCHFILE)
-end
-
-if exist('NIIPATH','var') ~= 1
-    NIIPATH=batch_file.datamat_creation.NIIPATH;
-end
-
-if exist('SAVEPATH','var') ~= 1
-    SAVEPATH=batch_file.datamat_creation.SAVEPATH;
+else template = load (BATCHFILE);
 end
 
 if exist(NIIPATH,'dir') ~= 7
@@ -54,47 +48,63 @@ end
 
 for i = 1:numel(subjID)
 
-batch_file.prefix=[batch_file.prefix, '_' ,subjID{i}];
+batch_files{i}.prefix=[template.batch_file.prefix, '_' ,subjID{i}];
 
 % data_files
-fprintf ('\nAltering data_files: \n')
+fprintf ('\nSetting data_files: \n')
 %batch_file.data_files{1,batch_file.datamat_creation.runs} = {};
 
-for j = 1: batch_file.datamat_creation.runs
+for j = 1: numel(template.batch_file.data_files)
 
-    [NIIpathname,NIIfilename, ext] = fileparts(batch_file.data_files{1,j});    
-%     % test if data_files possess correct nameing
-%     if isempty(findstr (NIIpathname, 'ID'))
-%         error ('template (batch_file.data_files): nifit path must contain wildcard "ID"')
-%     elseif isempty(findstr (NIIpathname, 'RUN'))
-%         error ('template (batch_file.data_files): nifit path must contain wildcard "RUN"')
-%     elseif isempty(findstr (NIIfilename, 'ID'))
-%         error ('template (batch_file.data_files): nifit filename must contain wildcard "ID"')
-%     elseif isempty(findstr (NIIfilename, 'RUN'))
-%         error ('template (batch_file.data_files): nifit filename must contain wildcard "RUN"')
-%     end
-        
-    NIIpathnameNEW = strrep(NIIpathname, 'ID', subjID{i});                       % replace wildcard 'ID' with subjID in PATH
-    NIIpathnameNEW = strrep(NIIpathnameNEW, 'RUN', ['run', num2str(j)]);         % replace wildcard 'RUN' with run in PATH       
-    NIIfilenameNEW = strrep(NIIfilename, 'ID', subjID{i});                       % replace wildcard 'ID' with subjID in NAME
-    NIIfilenameNEW = strrep(NIIfilenameNEW, 'RUN', ['run', num2str(j)]);         % replace wildcard 'RUN' with run in NAME
-    batch_file.data_files{1,j} = strcat(NIIPATH, '/',NIIfilenameNEW, ext);       % write new path and new filename
+     [NIIpathname,NIIfilename, ext] = fileparts(template.batch_file.data_files{1,j});    
+     % test if data_files possess correct nameing
+     if ~isempty (regexpi (NIIpathname, 'ID'))
+         NIIpathnameNEW = strrep(NIIpathname, 'ID', subjID{i});                     % replace wildcard 'ID' with subjID in PATH
+     %else fprintf ('no ID in template Nifti path (batch_file.data_files)\n')
+     end
+     
+     if ~isempty(regexpi (NIIpathname, 'RUN'))
+         NIIpathnameNEW = strrep(NIIpathnameNEW, 'RUN', ['run', num2str(j)]);         % replace wildcard 'RUN' with run in PATH       
+     %else fprintf ('no RUN in template Nifti path (batch_file.data_files)\n')
+     end
+     
+     if ~isempty(regexpi (NIIfilename, 'ID'))
+         NIIfilenameNEW = strrep(NIIfilename, 'ID', subjID{i});                       % replace wildcard 'ID' with subjID in NAME
+     else error ('template (batch_file.data_files): nifit filename must contain "ID"');
+     end
+     
+     if ~isempty(regexpi (NIIfilename, 'RUN'))
+         NIIfilenameNEW = strrep(NIIfilenameNEW, 'RUN', ['RUN', num2str(j)]);         % replace wildcard 'RUN' with run in NAME
+     %elseif fprintf ('no RUN in template Nifti filename (batch_file.data_files)\n')
+     end
+     
+     if ~isempty(regexpi (NIIfilename, 'run'))
+         NIIfilenameNEW = strrep(NIIfilenameNEW, 'run', ['run', num2str(j)]);         % replace wildcard 'RUN' with run in NAME
+     %elseif fprintf ('no run in template Nifti filename (batch_file.data_files)\n')
+     end
+
+    batch_files{i}.data_files{1,j} = strcat(NIIPATH, '/',NIIfilenameNEW, ext);       % write new path and new filename
     
-    fprintf ([(strcat(NIIpathname, '/',NIIfilename)) ' --> ', batch_file.data_files{1,j}, '\n'])    
+    fprintf ([(strcat(NIIpathname, '/',NIIfilename)) ' --> ', batch_files{i}.data_files{1,j}, '\n'])    
+
 end
 
+
+
 %% save
+
 [~ , BATCHfilename, ext] = fileparts(BATCHFILE);
  
 if findstr (BATCHfilename, 'template')
-   BATCHfilename = strrep(BATCHfilename, 'template', num2str(j));         % replace wildcard 'template' with subjID in template filename
-else BATCHfilename = strcat(BATCHfilename, '_', subjID{i});
+   BATCHfilename = strrep(BATCHfilename, 'template', subjID{i});         % replace wildcard 'template' with subjID in template filename
+else error ('template not in template batch file name')
 end
 
 
 fprintf (['save as:\n', SAVEPATH, '/', BATCHfilename, ext '\n'])
 save ([SAVEPATH, '/', BATCHfilename], 'batch_file');
 
-end
 
+end
+end
 
